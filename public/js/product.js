0380bd46da7db5b1c65dad4fc5e9e4fa4d327762
kerucko <cykinpavel@gmail.com>
2023-11-30 00:17:55 +0300

    edit product.js

--- public/js/product.js
+++ public/js/product.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
     console.log(id);
-    fetch("http://127.0.0.1:8000/product/" + id) // отправка GET-запроса на сервер
+    fetch("http://127.0.0.1:8080/product/" + id) // отправка GET-запроса на сервер
         .then(response => response.json()) // получение текстового ответа
         .then(data => {
             // JSON {name, price, description, shop}
