be0d2083865049179041598c76d4ac0451be1878
Iurii Kataev <iurii.kataev@gmail.com>
2023-11-29 21:55:38 +0300

    add: create database connection

--- app/cmd/store/main.go
+++ app/cmd/store/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "OnlineStore/internal/config"
+import (
+	"OnlineStore/internal/config"
+	"OnlineStore/internal/storage/postgres"
+	"fmt"
+	"log"
+)
 
 func main() {
 	cfg := config.MustLoad()
-	_ = cfg
-	// подключаем логер
-
-	// подлючаем базу данных
-
+	dbPath := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName)
+	db, err := postgres.New(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = db
 	// пишем обработчики
 }
