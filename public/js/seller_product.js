41953b5316d12f570e3f1b77fab1ea61dc6ad62c
Iurii Kataev <iurii.kataev@gmail.com>
2023-12-13 18:32:07 +0300

    new: display seller's product

---
+++ public/js/seller_product.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sellerID = urlParams.get('id');
+    fetch("http://127.0.0.1:8080/seller/product?id=" + sellerID) // отправка GET-запроса на сервер
+        .then(response => response.json()) // получение текстового ответа
+        .then(data => {
+            const main = document.getElementById("main");
+            for (let i = 0; i < data.length; i++) {
+                main.innerHTML += `
+                <div class="seller__products-card">
+                    <img
+                      class="seller__products-card--img"
+                      src = "${data[i].photo_path}"
+                      alt="product"
+                    />
+                    <div class="seller__img-name-price">
+                      <div class="seller__name-storage">
+                        <div class="seller__card-name">${data[i].title}</div>
+                        <div class="seller__card-storage--txt">Склад: ${data[i].store_address}</div>
+                      </div>
+                    </div>
+                    <div class="seller__card-quantity">
+                      Количество <br> на складе:
+                      <div class="seller__card-quantity--num">${data[i].amount}</div>
+                    </div>
+                </div>
+                `
+            }
+
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+});
